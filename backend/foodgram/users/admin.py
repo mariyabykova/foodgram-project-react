b2323a1df3e102e403a36fdd86978ca5a6a75508
Мария Быкова <mariabykova@MacBook-Pro-Maria.local>
2023-03-21 20:16:35 +0300

    В приложении users созданы модели и настроена админ-панель

--- backend/foodgram/users/admin.py
+++ backend/foodgram/users/admin.py
@@ -1,6 +1,22 @@
 from django.contrib import admin
 
-from .models import User
+from .models import User, Subscription
 
 
-admin.site.register(User)
+EMPTY_VALUE = '-пусто-'
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'email', 'username', 'first_name', 'last_name')
+    search_fields = ('username', 'email', 'first_name', 'last_name')
+    list_filter = ('username', 'email')
+    empty_value_display = EMPTY_VALUE
+
+
+@admin.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'user', 'author')
+    search_fields = ('user', 'author')
+    list_filter = ('user', 'author')
+    empty_value_display = EMPTY_VALUE
--- backend/foodgram/users/models.py
+++ backend/foodgram/users/models.py
@@ -1,20 +1,24 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from users.validators import validate_username
+
 
 class User(AbstractUser):
     email = models.EmailField(
+        'Электронная почта',
         max_length=254,
         unique=True,
         blank=False,
         null=False,
     )
     username = models.CharField(
-        'Имя пользоввателя',
+        'Имя пользователя',
         max_length=150,
         unique=True,
         blank=False,
         null=False,
+        validators=[validate_username, ]
     )
     first_name = models.CharField(
         'Имя',
@@ -29,10 +33,43 @@ class User(AbstractUser):
         null=False
     )
     password = models.CharField(
+        'Пароль',
         max_length=150,
         blank=False,
         null=False,
     )
 
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
     def __str__(self):
         return self.username
+
+
+class Subscription(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='follower',
+        verbose_name='Пользователь',
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='following',
+        verbose_name='Автор',
+    )
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user', 'author'],
+                name='unique_user_author'
+            )
+        ]
+        verbose_name = 'Подписка'
+        verbose_name_plural = 'Подписки'
+    
+    def __str__(self):
+        return f'{self.user.username} подписан на {self.author.username}'
