56a4a5e55a0ae8ed74f78627e521d1d62e323648
Мария Быкова <mariabykova@MacBook-Pro-Maria.local>
2023-03-21 20:26:36 +0300

    Зарегистрированы приложения recipes, api

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -31,6 +31,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users.apps.UsersConfig',
+    'recipes.apps.RecipesConfig',
+    'api.apps.ApiConfig',
 ]
 
 MIDDLEWARE = [
