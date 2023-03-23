e4e52754f1068ccc4d4b93cc14ecb8d3a95e8531
Мария Быкова <mariabykova@MacBook-Pro-Maria.local>
2023-03-23 20:43:09 +0300

    Настроена админ-панель для моделей из приложения recipes

--- backend/foodgram/recipes/admin.py
+++ backend/foodgram/recipes/admin.py
@@ -4,9 +4,48 @@ from recipes.models import (Favorite,  Ingredient, Recipe,
                              RecipeIngredient, ShoppingCart,  Tag)
 
 
-admin.site.register(Favorite)
-admin.site.register(Ingredient)
-admin.site.register(Recipe)
-admin.site.register(RecipeIngredient)
-admin.site.register(ShoppingCart)
-admin.site.register(Tag)
+EMPTY_VALUE = '-пусто-'
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'color', 'slug')
+    search_fields = ('name', 'color', 'slug')
+    list_filter = ('name', 'color', 'slug')
+    empty_value_display = EMPTY_VALUE
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'measurement_unit')
+    search_fields = ('name',)
+    list_filter = ('name',)
+    empty_value_display = EMPTY_VALUE
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'author')
+    search_fields = ('name', 'author')
+    list_filter = ('name', 'author', 'tags')
+    empty_value_display = EMPTY_VALUE
+
+
+@admin.register(RecipeIngredient)
+class RecipeIngredientAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'recipe', 'ingredient', 'amount')
+    empty_value_display = EMPTY_VALUE
+
+
+@admin.register(Favorite)
+class FavoriteAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'user', 'recipe')
+    search_fields = ('user', 'recipe')
+    empty_value_display = EMPTY_VALUE
+
+
+@admin.register(ShoppingCart)
+class ShoppingCartAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'user', 'recipe')
+    search_fields = ('user', 'recipe')
+    empty_value_display = EMPTY_VALUE
