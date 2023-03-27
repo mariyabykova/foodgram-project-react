4643f1d13641f0c3dfbf7702e928378187f0770c
mariyabykova <mariab@bk.ru>
2023-03-27 17:37:36 +0300

    Добавлен новый permission

--- backend/foodgram/api/permissions.py
+++ backend/foodgram/api/permissions.py
@@ -10,3 +10,15 @@ class CurrentUserOrSuperuserOrReadOnly(permissions.BasePermission):
               and request.method in permissions.SAFE_METHODS
               or request.user.is_superuser
               or request.user.is_staff))
+
+
+class IsAdminAuthorOrReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return (request.method in permissions.SAFE_METHODS
+                or request.user.is_authenticated)
+    
+    def has_object_permission(self, request, view, obj):
+        return (request.method in permissions.SAFE_METHODS
+                or request.user.is_superuser
+                or request.user.is_staff
+                or obj.author == request.user)
