3ecc793fc288475db398bf45fa8d7f095cae8e6a
mariyabykova <mariab@bk.ru>
2023-03-30 22:54:02 +0300

    Внесены изменения в urls

--- backend/foodgram/api/urls.py
+++ backend/foodgram/api/urls.py
@@ -7,13 +7,13 @@ from api.views import (IngredientViewSet, RecipeViewSet, TagViewSet,
 
 v1_router = DefaultRouter()
 
-v1_router.register(r'users/subscriptions', UserSubscriptionsViewSet, basename='subscriptions')
 v1_router.register(r'tags', TagViewSet, basename='tags')
 v1_router.register(r'ingredients', IngredientViewSet, basename='ingredients')
 v1_router.register(r'recipes', RecipeViewSet, basename='recipes')
 
 
 urlpatterns = [
+    path('users/subscriptions/', UserSubscriptionsViewSet.as_view({'get': 'list'})),
     path('users/<int:user_id>/subscribe/', UserSubscribeView.as_view()),
     path('', include(v1_router.urls)),
     path('', include('djoser.urls')),
