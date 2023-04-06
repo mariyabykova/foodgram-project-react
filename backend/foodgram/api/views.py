cfbf133a35acfaaf295e95f6bc7afac2f7815596
mariyabykova <mariab@bk.ru>
2023-04-06 14:42:04 +0300

    Исправлены ошибки pep8

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -3,7 +3,6 @@ from django_filters.rest_framework import DjangoFilterBackend
 from django.shortcuts import HttpResponse, get_object_or_404
 from rest_framework import mixins, status, viewsets
 from rest_framework.decorators import action
-from rest_framework.filters import SearchFilter
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
